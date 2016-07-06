02f9fbb1e03bebeb1fff9b97666e97384abf28cd
Dietrich Daroch <dietr1ch@acm.org>
2016-07-06 18:27:13 -0400

    Add fail stats on judge

--- judge.py
+++ judge.py
@@ -5,10 +5,17 @@ Judge
 
 from player_random import RandomPlayer
 from player_baier import BaierPlayer
+from player_uct_julio import JulioMonteCarlo
+from player_salas_a import Jorge_SalasA
+from player_salas_d import Jorge_SalasD
+from player_oliva import OlivaPlayer
+from player_wolf import WolfPlayer
+from player_felix import FelixPlayer
+from ivan_player import MapachePlayer
 from board import Board
 
 
-def play_match(white, black, time_limit=5):
+def play_match(white, black, time_limit=5, turn_limit=100):
     """
     Runs a single match
 
@@ -29,9 +36,17 @@ def play_match(white, black, time_limit=5):
     board = Board()
     plies = 0
 
-    # Run the match
-    while True:
+    if turn_limit < 100:
+        print("####################")
+        print("TURN LIMIT SET TO %d" % turn_limit)
+        print("####################")
+    if time_limit < 2:
+        print("--------------------")
+        print("TIME LIMIT SET TO %d" % time_limit)
+        print("--------------------")
 
+    # Run the match
+    for _ in range(turn_limit):
         # White plays
         print(board)
         board_copy = Board(board)  # Don't trust white
@@ -44,10 +59,10 @@ def play_match(white, black, time_limit=5):
             else:
                 # White can't move. Black won
                 print("%s won" % black_name)
-                return (black, plies)
+                return (black, plies, None)
         except Exception as e:
             print("%s won.  %s lost by Exception(%s)" % (black_name, white_name, e))
-            return (black, plies)
+            return (black, plies, white)
 
         # Update real board
         board = board.succ(queen, nq_x, nq_y, a_x, a_y)
@@ -64,55 +79,81 @@ def play_match(white, black, time_limit=5):
             else:
                 # Black can't move. White won
                 print("%s won" % white_name)
-                return (white, plies)
+                return (white, plies, None)
         except Exception as e:
             print("%s won.  %s lost by Exception(%s)" % (white_name, black_name, e))
-            return (white, plies)
+            return (white, plies, black)
 
         # Update real board
         board = board.succ(queen, nq_x, nq_y, a_x, a_y)
 
+    # Game terminated
+    print("Turn limit reached, no winner")
+    return (None, 0, None)
+
 
 try:
-    PLAYERS = [RandomPlayer]
-    RIVAL = BaierPlayer
+    PLAYERS = [OlivaPlayer,
+               JulioMonteCarlo,
+               Jorge_SalasA,
+               Jorge_SalasD,
+               MapachePlayer,
+               WolfPlayer,
+               FelixPlayer,
+               BaierPlayer
+    ]
+    RIVAL = RandomPlayer
+
+    SHAME = dict()
+    SHAME[RIVAL] = 0
+    SHAME[None] = 0
 
     GAMES = dict()
     GAMES[RIVAL] = 0
+    GAMES[None] = 0
 
     SCORES = dict()
     SCORES[RIVAL] = 0
+    SCORES[None] = 0
 
     for p in PLAYERS:
         GAMES[p] = 0
         SCORES[p] = 0
+        SHAME[p] = 0
 
 
-    ROUNDS = 2
-    TIME_LIMIT = 2
+    ROUNDS = 1
+    TURNS = 10
+    TIME_LIMIT = 1
     for player in PLAYERS:
 
         tied = True
         for _ in range(ROUNDS):
 
-            (winner1, _) = play_match(RIVAL, player, TIME_LIMIT)
+            (winner1, _, s) = play_match(RIVAL, player, TIME_LIMIT, TURNS)
             SCORES[winner1] += 1
             GAMES[player] += 1
             GAMES[RIVAL] += 1
+            if s:
+                SHAME[s] += 1
 
-            (winner2, _) = play_match(player, RIVAL, TIME_LIMIT)
+            (winner2, _, s) = play_match(player, RIVAL, TIME_LIMIT, TURNS)
             SCORES[winner2] += 1
             GAMES[player] += 1
             GAMES[RIVAL] += 1
+            if s:
+                SHAME[s] += 1
 
             if winner1 == winner2:
                 tied = False
 
         if tied:
-            (winner3, _) = play_match(RIVAL, player, TIME_LIMIT)
+            (winner3, _, s) = play_match(RIVAL, player, TIME_LIMIT, TURNS)
             SCORES[winner3] += 1
             GAMES[player] += 1
             GAMES[RIVAL] += 1
+            if s:
+                SHAME[s] += 1
 
 except KeyboardInterrupt:
     pass
@@ -122,3 +163,10 @@ print("Results:")
 print("  * %20s: %d/%d" % (RIVAL.__name__, SCORES[RIVAL], GAMES[RIVAL]))
 for p in PLAYERS:
     print("  * %20s: %d/%d" % (p.__name__, SCORES[p], GAMES[p]))
+print("Unfinished games: %d" % GAMES[None])
+
+print("Shame:")
+print("  =( %20s: %d" % (RIVAL.__name__, SHAME[RIVAL]))
+for p in PLAYERS:
+    if SHAME[p] > 0:
+        print("  =( %20s: %d" % (p.__name__, SHAME[p]))
